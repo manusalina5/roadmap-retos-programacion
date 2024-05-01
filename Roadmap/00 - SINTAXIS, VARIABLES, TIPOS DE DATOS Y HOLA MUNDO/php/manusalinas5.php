194e7504fa919146be3968243a354e0d53829b2a
Juan Manuel Salinas <salinasjuanmanuel98@gmail.com>
2024-05-01 02:49:59 -0300

    #00 - PHP
    Solución al reto #00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/php/manusalinas5.php
@@ -0,0 +1,24 @@
+<?php
+
+// https://www.php.net
+
+# Comentario
+
+/*
+Comentario 
+En
+Varias
+Lineas
+*/ 
+
+$variable1 = "Hola mundo";
+
+define("lenguaje","PHP");
+
+$bool = true; // booleano
+$string = "Hola"; // cadena de texto
+$int = 1; // enteros
+$float = 1.5; // número de punto flotante
+$array = ["uno","dos","tres"]; // array
+
+echo "¡Hola, ".lenguaje."!";
